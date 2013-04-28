6fd8fccfb58a75132ff848cc7d10d46357116cfb
G@mOBEP <rusmiran@gmail.com>
2013-04-28 14:24:21 +0400

    добавлен DBuilder

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
  * Time: 16:40
  */
 
-exports.fs      = require('./lib/fs');
-exports.package = require('./lib/package');
-exports.string  = require('./lib/string');
-exports.type    = require('./lib/type');
+exports.DBuilder = require('./lib/dbuilder').DBuilder;
+exports.fs       = require('./lib/fs');
+exports.package  = require('./lib/package');
+exports.string   = require('./lib/string');
+exports.type     = require('./lib/type');
